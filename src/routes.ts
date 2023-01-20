2f81ca575cd5729768ee3390bd30ec8bc8b0b25d
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-20 18:53:48 -0300

    feat: add route to toggle habit status

--- src/routes.ts
+++ src/routes.ts
@@ -62,12 +62,61 @@ export async function appRoutes(app: FastifyInstance) {
     });
 
     const completedHabits = day?.dayHabits.map((dayHabit) => {
-      return dayHabit.day_id;
+      return dayHabit.habit_id;
     });
 
     return {
       possibleHabits,
       completedHabits,
     };
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findFirst({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+    console.log("dayhabit: ", dayHabit);
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
   });
 }
